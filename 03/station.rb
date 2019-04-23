0fbd49bb042d0aaf2ff2ff838e020e11e043ed1d
Yegor Tikhanin <yegor.tikhanin@gmail.com>
2019-04-23 19:14:20 +0300

    split & commnents

---
+++ 03/station.rb
@@ -0,0 +1,20 @@
+class Station
+  attr_reader :trains, :name
+
+  def initialize(name)
+    @name = name
+    @trains = []
+  end
+
+  def add_train(train)
+    @trains << train
+  end
+
+  def delete_train(train)
+    @trains.delete(train)
+  end
+
+  def get_trains_of_type(type)
+    @trains.select { |train| train.type == type }.size
+  end
+end
---
+++ 03/train.rb
@@ -0,0 +1,75 @@
+class Train
+  attr_reader :speed, :wagons, :number
+
+  def initialize(number, type, wagons)
+    @number = number
+    @type = type
+    @wagons = wagons
+  end
+
+  def speed_up
+    self.speed += 5
+  end
+
+  def speed_down
+    self.speed -= 5
+    self.speed = 0 if speed < 0
+  end
+
+  def stop
+    self.speed = 0
+  end
+
+  def go
+    self.speed = 5
+    # убрать поезд из списка станции
+  end
+
+  def increment_wagon
+    @wagons += 1 if speed == 0
+  end
+
+  def decrement_wagon
+    @wagons -= 1 if speed == 0 && wagons > 0
+  end
+
+  def route=(route)
+    @route = route
+    @current_index_station = 0
+    to_start_station(self)
+  end
+
+  def to_start_station(train)
+    @route.stations.each { |station| station.trains.delete(train) }
+    @route.stations[0].add_train(train)
+  end
+
+  def to_next_station
+    unless @route.stations[@current_index_station + 1].nil?
+      p @route.stations[@current_index_station + 1]
+      @current_index_station += 1
+      @route.stations.each { |station| station.trains.delete(self) }
+      @route.stations[@current_index_station].add_train(self)
+    end
+  end
+
+  def to_previous_station
+    unless @route.stations[@current_index_station - 1].nil?
+      @current_index_station -= 1
+      @route.stations.each { |station| station.trains.delete(self) }
+      @route.stations[@current_index_station].add_train(self)
+    end
+  end
+
+  def show_current_station
+    @route.stations[@current_index_station]
+  end
+
+  def show_next_station
+    @route.stations[@current_index_station + 1]
+  end
+
+  def show_previous_station
+    @route.stations[@current_index_station - 1]
+  end
+end
