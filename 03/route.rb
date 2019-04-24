f2bb21ae5d51a2c83c06dec18531a781da3038b2
Yegor Tikhanin <yegor.tikhanin@gmail.com>
2019-04-24 12:16:45 +0300

    by comments

--- 03/route.rb
+++ 03/route.rb
@@ -1,5 +1,5 @@
 class Route
-  attr_reader :start_point, :stations
+  attr_reader :start_point, :end_point, :stations
 
   def initialize(start_point, end_point)
     @start_point = start_point
@@ -18,40 +18,4 @@ class Route
   def show_stations
     @stations.each { |station| puts station.name }
   end
-
-  def to_start_station(train)
-    @stations.each { |station| station.trains.delete(train) }
-    @stations[0].add_train(train)
-  end
-
-  # def to_next_station(train)
-  #   index_station = get_index_station(train)
-  #   @stations.each { |station| station.trains.delete(train) }
-  #   @stations[index_station + 1].add_train(train) unless @stations[index_station + 1].nil?
-  # end
-
-  # def to_previous_station(train)
-  #   index_station = get_index_station(train)
-  #   @stations.each { |station| station.trains.delete(train) }
-  #   @stations[index_station - 1].add_train(train) unless @stations[index_station - 1].nil?
-  # end
-
-  # def get_index_station(train)
-  #   @stations.index { |station| station.trains.include?(train) }
-  # end
-
-  # def show_station(train)
-  #   index_station = get_index_station(train)
-  #   puts @stations[index_station].name
-  # end
-
-  # def show_next_station(train)
-  #   index_station = get_index_station(train)
-  #   puts @stations[index_station + 1].name unless @stations[index_station + 1].nil?
-  # end
-
-  # def show_previous_station(train)
-  #   index_station = get_index_station(train)
-  #   puts @stations[index_station - 1].name unless @stations[index_station - 1].nil?
-  # end
 end
--- 03/train.rb
+++ 03/train.rb
@@ -7,69 +7,57 @@ class Train
     @wagons = wagons
   end
 
-  def speed_up
-    self.speed += 5
+  def speed_up(speed)
+    @speed += speed
   end
 
   def speed_down
-    self.speed -= 5
-    self.speed = 0 if speed < 0
+    @speed -= speed
+    @speed = 0 if @speed < 0
   end
 
   def stop
-    self.speed = 0
-  end
-
-  def go
-    self.speed = 5
-    @route.stations.each { |station| station.trains.delete(self) }
+    @speed = 0
   end
 
   def increment_wagon
-    @wagons += 1 if speed == 0
+    @wagons += 1 if @speed == 0
   end
 
   def decrement_wagon
-    @wagons -= 1 if speed == 0 && wagons > 0
+    @wagons -= 1 if @speed == 0 && wagons > 0
   end
 
   def route=(route)
-    @route = route
+    current_station.delete_train(self) unless @route.nil?
     @current_index_station = 0
-    to_start_station(self)
-  end
-
-  def to_start_station(train)
-    @route.stations.each { |station| station.trains.delete(train) }
-    @route.stations[0].add_train(train)
+    @route = route
+    current_station.add_train(self)
   end
 
   def to_next_station
-    unless @route.stations[@current_index_station + 1].nil?
-      p @route.stations[@current_index_station + 1]
-      @current_index_station += 1
-      @route.stations.each { |station| station.trains.delete(self) }
-      @route.stations[@current_index_station].add_train(self)
-    end
+    return if next_station.nil?
+    current_station.delete_train(self)
+    next_station.add_train(self)
+    @current_index_station += 1
   end
 
   def to_previous_station
-    unless @route.stations[@current_index_station - 1].nil?
-      @current_index_station -= 1
-      @route.stations.each { |station| station.trains.delete(self) }
-      @route.stations[@current_index_station].add_train(self)
-    end
+    return if previous_station.nil?
+    current_station.delete_train(self)
+    previous_station.add_train(self)
+    @current_index_station -= 1
   end
 
-  def show_current_station
+  def current_station
     @route.stations[@current_index_station]
   end
 
-  def show_next_station
+  def next_station
     @route.stations[@current_index_station + 1]
   end
 
-  def show_previous_station
-    @route.stations[@current_index_station - 1]
+  def previous_station
+    @route.stations[@current_index_station - 1] if @current_index_station - 1 >= 0
   end
 end
